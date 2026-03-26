72269ad51dfa06a0c2c1a3aeba024357709e457d
DANIA-DESKTOP\user <ainuldania@gmail.com>
2026-03-26 21:56:29 +0800

    Added appsettings.json config file and test the connection

---
+++ DatabaseManager.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+namespace Flashcards
+{
+    internal class DatabaseManager
+    {
+        //Appsetting.json config connection
+        public string GetConnectionString()
+        {
+            IConfiguration config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+            string connectionString = config.GetConnectionString("DefaultConnection");
+
+            return connectionString;
+
+        }
+
+        public bool TestConnection()
+        {
+            try
+            {
+                using (var connection = new SqlConnection(GetConnectionString()))
+                {
+                    connection.Open();
+                    return true; // Connection successful
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Database connection failed: {ex.Message}");
+                return false; // Connection failed
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //In case of any issues with encoding, this will ensure that the console can handle UTF-8 characters properly.
+            Console.InputEncoding = System.Text.Encoding.UTF8;
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            DatabaseManager databaseManager = new DatabaseManager();
+            databaseManager.TestConnection();
         }
     }
 }
